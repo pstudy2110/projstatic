82684a262c486de62c58dcf5b089a3945bd3c978
pstudy2110 <60999442+pstudy2110@users.noreply.github.com>
2025-01-29 19:25:37 -0600

    Add files via upload

---
+++ web_sock_static/nav.js
@@ -0,0 +1,46 @@
+const ws = new WebSocket('ws://localhost:8089/home');
+const navResponses = document.querySelector(".nav-responses");
+ws.addEventListener("open", (event) => {
+    console.log("Connected to WebSocket server:", event);
+});
+
+ws.addEventListener("message", (event) => {
+    console.log("Message received from server:", event.data);
+    // Create a new message element
+    const messageElement = document.createElement("div");
+    messageElement.textContent = event.data;
+
+    // Append the message to the chat log
+    navResponses.appendChild(messageElement);
+
+    // Scroll to the bottom of the chat log
+    navResponses.scrollTop = navResponses.scrollHeight;
+});
+
+ws.addEventListener("close", (event) => {
+    console.log("Disconnected from WebSocket server:", event);
+});
+
+ws.addEventListener("error", (event) => {
+    console.error("WebSocket error:", event);
+});
+
+
+function navOn() { 
+    document.querySelector(".nav-responses").classList.toggle("hidden");
+}
+
+
+function sendMessage() {
+    const messageTextarea = document.getElementById("messageInput");
+    const message = messageTextarea.value;
+    ws.send(message);
+    if (message) {
+        messageTextarea.value = ""; // Clear the textarea
+    } else {
+        console.log("Please enter a message");
+    }
+    console.log("Message sent:", message);
+
+}
+
